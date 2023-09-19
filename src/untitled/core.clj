3810b4a40d199c9df6a3c1295e4e154219735a7c
Dmitriy Korolev <58227907+Ta3ikNSU@users.noreply.github.com>
2023-09-20 01:37:49 +0700

    порешал фор в 1.1

--- src/untitled/core.clj
+++ src/untitled/core.clj
@@ -5,13 +5,19 @@
   [x]
   (println x "Hello, World!"))
 ; ----------------------------------------------------------------------------
-(defn gen-permutations-1-1 [n alphabet]
-  (if (zero? n)
-    [""]
-    (for [symbol alphabet
-          prev (gen-permutations-1-1 (dec n) (remove #{symbol} alphabet))
-          :when (not= symbol (first prev))]
-      (str symbol prev))
+(defn remove-element [my-vector element-to-remove]
+  (filter #(not (= % element-to-remove)) my-vector))
+
+(defn generate-string [array n answers current need-length]
+  (if (= (count current) need-length)
+    (println current)
+    (if (= n -1)
+      answers
+      (let [new-list-without-element (remove-element array (nth array n))]
+          (generate-string new-list-without-element (- (count new-list-without-element) 1) answers (str current (nth array n)) need-length)
+          (generate-string array (- n 1) answers current need-length)
+        )
+      )
     )
   )
 
@@ -21,10 +27,7 @@
     (let [alphabet (butlast args)]
       (if (> n (count alphabet))
         (throw (Exception. "n больше размера алфавита")))
-      (println n)
-      (println alphabet)
-      (println (gen-permutations-1-1 n alphabet))
-      )
-    )
-  )
+      (let [answers (generate-string alphabet (- (count alphabet) 1) [] "" n)]
+        (doseq [answer answers]
+          (println answer))))))
 ; ----------------------------------------------------------------------------
