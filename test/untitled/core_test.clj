1614b07f684e1f7c64ffeea26b08b32eb78c0bd5
Dmitriy Korolev <58227907+Ta3ikNSU@users.noreply.github.com>
2023-10-12 00:07:34 +0700

    1.3 + тесты

--- test/untitled/core_test.clj
+++ test/untitled/core_test.clj
@@ -36,3 +36,19 @@
     (is (= (ta3ik/generate-sequences-recur [] ["a" "b" "c"] 3) ["aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc"]))
     (is (= (ta3ik/generate-sequences-recur [] ["1" "2" "3"] 1) ["1" "2" "3"]))))
 
+(deftest test-my-map
+  (testing "my-map should map a function to each element in the collection"
+    (is (= (ta3ik/my-map inc '(1 2 3 4 5)) '(2 3 4 5 6)))
+    (is (= (ta3ik/my-map str '(a b c d e)) '("a" "b" "c" "d" "e")))
+    (is (= (ta3ik/my-map (fn [x] (* x x)) '(1 2 3 4 5)) '(1 4 9 16 25)))
+    (is (= (ta3ik/my-map (fn [x] (str "item" x)) '(1 2 3)) '("item1" "item2" "item3")))
+    (is (= (ta3ik/my-map identity '()) '()))))
+
+(deftest test-my-filter
+  (testing "my-filter should filter elements in the collection based on a predicate"
+    (is (= (ta3ik/my-filter even? '(1 2 3 4 5 6)) '(2 4 6)))
+    (is (= (ta3ik/my-filter odd? '(1 2 3 4 5 6)) '(1 3 5)))
+    (is (= (ta3ik/my-filter #(> % 2) '(1 2 3 4 5)) '(3 4 5)))
+    (is (= (ta3ik/my-filter #(<= % 3) '(1 2 3 4 5)) '(1 2 3)))
+    (is (= (ta3ik/my-filter #(= % 0) '()) '()))))
+
